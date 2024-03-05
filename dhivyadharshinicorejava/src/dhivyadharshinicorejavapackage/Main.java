182c30ebc72612ee34e9a0ed5e28722469cba129
Lenovo <Lenovo@DESKTOP-NKOLHH0>
2024-03-05 15:44:27 +0530

    It's my core java practical program.

---
+++ dhivyadharshinicorejava/src/dhivyadharshinicorejavapackage/Main.java
@@ -0,0 +1,15 @@
+package dhivyadharshinicorejavapackage;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner inp=new Scanner(System.in);
+		int a=inp.nextInt();
+		int b=inp.nextInt();
+		int c=a+b;
+		System.out.println("Addition of a and b is "+c);
+
+	}
+
+}
